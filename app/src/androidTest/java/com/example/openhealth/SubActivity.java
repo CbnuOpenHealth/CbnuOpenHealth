2609c35d0afac271661006a64eb4f2b81f54e8c3
tw <kimxordnjs@naver.com>
2019-11-11 17:09:37 +0900

    first upload

---
+++ app/src/androidTest/java/com/example/openhealth/SubActivity.java
@@ -0,0 +1,6 @@
+package com.example.openhealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SubActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/openhealth/SubActivityWalking.java
@@ -0,0 +1,14 @@
+package com.example.openhealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SubActivityWalking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub_walking);
+    }
+}
