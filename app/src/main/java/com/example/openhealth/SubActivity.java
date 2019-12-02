79297a79e1f4093c7f72dc721479e161aa56b1e7
jungchuen <yllowrose452@naver.com>
2019-12-02 15:40:37 +0900

    add SubActivity

--- app/src/main/java/com/example/openhealth/SubActivity.java
+++ app/src/main/java/com/example/openhealth/SubActivity.java
@@ -2,33 +2,69 @@ package com.example.openhealth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class SubActivity extends AppCompatActivity {
+
+    private TextView mTextViewPulse;
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
 
-        final Button pulseButton = (Button) findViewById(R.id.pulseButton);
+        mTextViewPulse = findViewById(R.id.pulseResult);
+        Button pulseButton = findViewById(R.id.pulseButton);
 
+        mQueue = Volley.newRequestQueue(this);
         pulseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String pulseData;
+                String url = "http://192.168.0.6/get.php";
+                JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    JSONArray jsonArray = response.getJSONArray("pulse");
+                                    for (int i = 0; i < jsonArray.length(); i++){
+                                        JSONObject object = jsonArray.getJSONObject(i);
+                                        int datas = object.getInt("datas");
+                                        mTextViewPulse.append(String.valueOf(datas));
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
 
-
-               // RequestPulse requestPulse = new RequestPulse(pulseData, responseListener);
-               // RequestQueue queue = Volley.newRequestQueue(SubActivity.this);
-               // queue.add(requestPulse);
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                });
+                mQueue.add(request);
 
             }
         });
+
     }
+
 }
